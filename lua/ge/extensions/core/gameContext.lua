ced52febd1675debb7b95191889b14106f319381
TristanGaldys <123184923+TristanGaldys@users.noreply.github.com>
2025-06-24 12:45:34 -0400

    - v2.4 Beta

--- lua/ge/extensions/core/gameContext.lua
+++ lua/ge/extensions/core/gameContext.lua
@@ -32,7 +32,7 @@ local function getWIPWarningLabel()
       end
     end
   if career_career.isActive() then
-    return "RLS Career Overhaul v2.4 Alpha - WIP"
+    return "RLS Career Overhaul v2.4 Beta - WIP"
   end
   return nil
 end
